b674401a9a4e5ed394d93d51441b4bc54880b313
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-10-18 22:25:55 +0400

    feat: add new pest and pagination

--- script.js
+++ script.js
@@ -22,3 +22,48 @@ function mouseMove(e) {
 
   word.style.textShadow = `${xWalk}px ${yWalk * -1}px 5px rgba(140,86,20, .3)`;
 }
+
+const table = document.getElementById('projects-table');
+const paginationDiv = document.getElementById('pagination');
+const rowsPerPage = 4;
+
+function displayPage(page) {
+  const startIdx = (page - 1) * rowsPerPage;
+  const endIdx = startIdx + rowsPerPage;
+  const rows = table.getElementsByTagName('tr');
+
+  for (let i = 0; i < rows.length; i++) {
+    if (i >= startIdx && i < endIdx) {
+      rows[i].style.display = 'table-row';
+    } else {
+      rows[i].style.display = 'none';
+    }
+  }
+
+  table.scrollIntoView({ behavior: 'smooth' });
+
+  const buttons = paginationDiv.getElementsByTagName('button');
+  for (let i = 0; i < buttons.length; i++) {
+    if (i + 1 === page) {
+      buttons[i].classList.add('active-button');
+    } else {
+      buttons[i].classList.remove('active-button');
+    }
+  }
+}
+
+const totalRows = table.rows.length - 1; // Exclude the header row
+const totalPages = Math.ceil(totalRows / rowsPerPage);
+
+
+for (let i = 1; i <= totalPages; i++) {
+  const link = document.createElement('button');
+  link.textContent = i;
+  link.addEventListener('click', () => {
+    displayPage(i);
+  });
+  paginationDiv.appendChild(link);
+}
+
+// Display the first page by default
+displayPage(1);
